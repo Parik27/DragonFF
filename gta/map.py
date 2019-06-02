eb4c4e2c71a7eec60fc8e1d134649a9117e3af4e
Jan Bláha <blaha.j502@gmail.com>
2019-06-02 19:54:19 +0200

    Removed old code

--- gta/map.py
+++ gta/map.py
@@ -97,22 +97,19 @@ class IPLSection_CULL(IPLSection):
 #######################################################
 class IPL:
 
+    sections = {
+        'inst': IPLSection_INST,
+        'pick': IPLSection_PICK,
+        'cull': IPLSection_CULL,
+        'zone': IPLSection_ZONE
+    }
+
     #######################################################
     def __init__(self):	
-
         self.inst_list = []
         self.pickup_list = []
         self.cull_list = []
         self.zone_list = []
-
-        self.sections = {
-            'inst': IPLSection_INST,
-            'pick': IPLSection_PICK,
-            'cull': IPLSection_CULL,
-            'zone': IPLSection_ZONE
-        }
-
-        self.currentSectionName = None
 
     #######################################################
     def read(self, filename):
@@ -136,107 +133,3 @@ class IPL:
         print(self.pickup_list)
         print(self.cull_list)
         print(self.zone_list)
-
-
-
-# Experimentals... for future reference
-
-# class IPLModel:
-# 	def __init__(self):
-# 		self.id = None
-# 		self.model = None
-# 		self.posX = 0
-# 		self.posY = 0
-# 		self.posZ = 0
-# 		self.scaleX = 1
-# 		self.scaleY = 1
-# 		self.scaleZ = 1
-# 		self.rotX = 0
-# 		self.rotY = 0
-# 		self.rotZ = 0
-# 		self.rotW = 1
-
-# class IPLZone:
-
-# 	zone_gang_count = 13
-
-# 	def __init__(self):
-# 		self.name = None
-# 		self.type = None
-# 		self.minX = 0
-# 		self.minY = 0
-# 		self.minZ = 0
-# 		self.maxX = 0
-# 		self.maxY = 0
-# 		self.maxZ = 0
-# 		self.island = None
-# 		self.gangCarDensityDay = [0] * IPLZone.zone_gang_count
-# 		self.gangCarDensityNight = [0] * IPLZone.zone_gang_count
-# 		self.gangDensityDay = [0] * IPLZone.zone_gang_count
-# 		self.gangDensityNight = [0] * IPLZone.zone_gang_count
-# 		self.pedGroupDay = 0
-# 		self.pedGroupNight = 0
-
-# class IPLSectionLoader_INST(IPLSectionLoaderBase):
-
-# 	def processLine(self, line):
-# 		params = line.split(",")
-
-# 		model = IPLModel()
-# 		model.id =     params[0].strip()
-# 		model.model =  params[1].strip()
-# 		model.posX =   params[2].strip()
-# 		model.posY =   params[3].strip()
-# 		model.posZ =   params[4].strip()
-# 		model.scaleX = params[5].strip()
-# 		model.scaleY = params[6].strip()
-# 		model.scaleZ = params[7].strip()
-# 		model.rotX =   params[8].strip()
-# 		model.rotY =   params[9].strip()
-# 		model.rotZ =   params[10].strip()
-# 		model.rotW =   params[11].strip()
-
-# 		self.iplLoader.inst_list.append(model)
-
-# class IPLSectionLoader_ZONE(IPLSectionLoaderBase):
-
-# 	def processLine(self, line):
-# 		params = line.split(",")
-
-# 		zone = IPLZone()
-# 		zone.name =   params[0].strip()
-# 		zone.type =   params[1].strip()
-# 		zone.minX =   params[2].strip()
-# 		zone.minY =   params[3].strip()
-# 		zone.minZ =   params[4].strip()
-# 		zone.maxX =   params[5].strip()
-# 		zone.maxY =   params[6].strip()
-# 		zone.maxZ =   params[7].strip()
-# 		zone.island = params[8].strip()
-
-# 		self.iplLoader.zone_list.append(zone)
-
-# class IPLSectionLoader_PICK(IPLSectionLoaderBase): pass
-
-# class IPLSectionLoader_CULL(IPLSectionLoaderBase): pass
-
-
-        # inst = IPLModel()
-        # inst.id =     params[0].strip()
-        # inst.model =  params[1].strip()
-        # inst.posX =   params[2].strip()
-        # inst.posY =   params[3].strip()
-        # inst.posZ =   params[4].strip()
-        # inst.scaleX = params[5].strip()
-        # inst.scaleY = params[6].strip()
-        # inst.scaleZ = params[7].strip()
-        # inst.rotX =   params[8].strip()
-        # inst.rotY =   params[9].strip()
-        # inst.rotZ =   params[10].strip()
-        # inst.rotW =   params[11].strip()
-
-        # lineData = Section_INST()
-        # for i in range(len(Section_INST)):
-        # 	lineData[i] = params[i]
-            
-        # self.iplLoader.inst_list.append(model)
