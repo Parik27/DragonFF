f98cb7b8d9d2c35f5f1dea7712a17f1e3f77e1b4
Psycrow101 <psycrow101@gmail.com>
2025-07-13 16:31:46 +0300

    Added material reflection and specular presets Thanks to ByteKun

--- gui/dff_menus.py
+++ gui/dff_menus.py
@@ -116,6 +116,8 @@ class MATERIAL_PT_dffMaterials(bpy.types.Panel):
         box.row().prop(settings, "export_reflection")
 
         if settings.export_reflection:
+            box.prop(settings, "preset_reflection_scales", text="Scale Preset", icon="PRESET", icon_only=True)
+
             self.draw_labelled_prop(
                 box.row(), settings, ["reflection_scale_x", "reflection_scale_y"],
                 "Scale"
@@ -124,9 +126,10 @@ class MATERIAL_PT_dffMaterials(bpy.types.Panel):
                 box.row(), settings, ["reflection_offset_x", "reflection_offset_y"],
                 "Offset"
             )
-            self.draw_labelled_prop(
-                box.row(), settings, ["reflection_intensity"], "Intensity"
-            )
+
+            row = box.row()
+            row.prop(settings, "reflection_intensity", text="Intensity")
+            row.prop(settings, "preset_reflection_intensities", text="", icon="PRESET", icon_only=True)
 
     #######################################################
     def draw_specl_box(self, context, box):
@@ -135,11 +138,12 @@ class MATERIAL_PT_dffMaterials(bpy.types.Panel):
         box.row().prop(settings, "export_specular")
 
         if settings.export_specular:
-            self.draw_labelled_prop(
-                box.row(), settings, ["specular_level"], "Level"
-            )
+            row = box.row()
+            row.prop(settings, "specular_level", text="Level")
+            row.prop(settings, "preset_specular_levels", text="", icon="PRESET", icon_only=True)
+
             box.row().prop(settings, "specular_texture", text="Texture")
-        
+
     #######################################################
     def draw_mesh_menu(self, context):
 
@@ -196,7 +200,25 @@ class MATERIAL_PT_dffMaterials(bpy.types.Panel):
 
         except Exception as e:
             print(e)
-        
+
+    #######################################################
+    # Callback function from preset_reflection_intensities enum
+    def set_preset_reflection_intensity(self, context):
+        self.reflection_intensity = float(self.preset_reflection_intensities)
+
+    #######################################################
+    # Callback function from preset_reflection_scales enum
+    def set_preset_reflection_scale(self, context):
+        val = float(self.preset_reflection_scales)
+        self.reflection_scale_x = val
+        self.reflection_scale_y = val
+        self.reflection_offset_x = val
+
+    #######################################################
+    # Callback function from preset_specular_levels enum
+    def set_preset_specular_level(self, context):
+        self.specular_level = float(self.preset_specular_levels)
+
     #######################################################
     def draw(self, context):
 
@@ -491,11 +513,78 @@ class DFFMaterialProps(bpy.types.PropertyGroup):
     reflection_offset_x  : bpy.props.FloatProperty ()
     reflection_offset_y  : bpy.props.FloatProperty ()
     reflection_intensity : bpy.props.FloatProperty ()
-    
+
+    # Pre-set Reflection Intensity
+    preset_reflection_intensities : bpy.props.EnumProperty(
+        items =
+        (
+            ("0.0", "Off", "Reflection is disabled (0.0)"),
+            *(
+                ("%.2f" % (i / 100), "ENV %d" % i, "Reflection level %d" % i)
+                for i in range(1, 51)
+            ),
+            ("1.0", "Mirror", "Mirror effect (1.0)"),
+            ("3.0", "Chrome", "Chrome surface (3.0)"),
+        ),
+        name = "Preset Reflection Intensity",
+        description = "Preset reflection intensity levels (0-50)",
+        update = MATERIAL_PT_dffMaterials.set_preset_reflection_intensity
+    )
+
+    # Pre-set Reflection Scale & Offset
+    preset_reflection_scales : bpy.props.EnumProperty(
+        items =
+        (
+            ("0.0", "OFF SPEC", "Glare is disabled"),
+            ("0.20", "SPEC 51", "51 Glare"),
+            ("0.40", "SPEC 102", "102 Glare"),
+            ("0.60", "SPEC 153", "153 Glare"),
+            ("0.80", "SPEC 204", "204 Glare"),
+            ("1.0", "MAX SPEC ", "255 Glare"),
+        ),
+        name = "Preset Reflection Scales",
+        description = "Preset reflection offsets and scales",
+        update = MATERIAL_PT_dffMaterials.set_preset_reflection_scale
+    )
+
     # Specularity
     export_specular  : bpy.props.BoolProperty(name="Specular Material")
     specular_level   : bpy.props.FloatProperty  ()
     specular_texture : bpy.props.StringProperty ()
+
+    # Pre-set Specular Level
+    preset_specular_levels : bpy.props.EnumProperty(
+        items =
+        (
+            ("0.0", "Off", "Disabled (0.0)"),
+            ("0.99", "60", "Specular (0.99)"),
+            ("0.87", "61", "Specular (0.87)"),
+            ("0.79", "62", "Specular (0.79)"),
+            ("0.69", "63", "Specular (0.69)"),
+            ("0.63", "64", "Specular (0.63)"),
+            ("0.56", "65", "Specular (0.56)"),
+            ("0.50", "66", "Specular (0.50)"),
+            ("0.44", "67", "Specular (0.44)"),
+            ("0.39", "68", "Specular (0.39)"),
+            ("0.34", "69", "Specular (0.34)"),
+            ("0.30", "70", "Specular (0.30)"),
+            ("0.26", "71", "Specular (0.26)"),
+            ("0.20", "72", "Specular (0.20)"),
+            ("0.19", "73", "Specular (0.19)"),
+            ("0.16", "74", "Specular (0.16)"),
+            ("0.14", "75", "Specular (0.14)"),
+            ("0.12", "76", "Specular (0.12)"),
+            ("0.09", "77", "Specular (0.09)"),
+            ("0.08", "78", "Specular (0.08)"),
+            ("0.06", "79", "Specular (0.06)"),
+            ("0.04", "80", "Specular (0.04)"),
+            ("0.03", "81", "Specular (0.03)"),
+            ("0.01", "82", "Specular (0.01)"),
+        ),
+        name = "Preset Specular Levels",
+        description = "Preset specular levels",
+        update = MATERIAL_PT_dffMaterials.set_preset_specular_level
+    )
 
     # Collision Data
     col_flags       : bpy.props.IntProperty(min=0, max=255)
@@ -537,6 +626,8 @@ class DFFMaterialProps(bpy.types.PropertyGroup):
             ("[0, 17, 255, 255]", "ImVehFT - Light All-day", ""),
             ("[0, 18, 255, 255]", "ImVehFT - Default Day", "")
         ),
+        name = "Preset Material Colors",
+        description = "Preset material colors",
         update = MATERIAL_PT_dffMaterials.set_preset_color
     )
         
